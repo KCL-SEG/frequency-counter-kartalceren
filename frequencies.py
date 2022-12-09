edf0626e9369d3a72dd5a8757597036ad8527e50
Ceren Kartal (kartalceren) <ceren.s.kartal@kcl.ac.uk>
2022-12-09 13:17:15 +0000

    all frequency tests passed

--- frequencies.py
+++ frequencies.py
@@ -1,14 +1,16 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-from collections import Counter
 
 def frequencies(items):
 
- frequencies={} 
- for item in items:
-    frequencies[item] = frequencies.get(item, 0) + 1
- 
- for key, value in frequencies.items():
-    print(key, value)
- 
+ frequencies={}
+
+ list = [str(n) for n in items ]
+
+ for i in list:
+    if i in frequencies:
+     frequencies[i] += 1
+    else:
+     frequencies[i]=1
+
  return frequencies
